19df8448652efcd40ec978413c6788d581ee9820
lenovo <lenovo@LAPTOP-TNSNIB6O>
2024-09-11 23:11:44 +0530

    Add project files.

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,34 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _dbContext;
+        public CategoryController(ApplicationDBContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+             List<Category> categories = _dbContext.Categories.ToList();
+            return View(categories);
+            //return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _dbContext.Categories.Add(obj);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
